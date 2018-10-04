e23e8eb1bbee21e6fe4996cff0c5a13e5cd432c2
NycollSM <nycollsotomorales21@gmail.com>
2018-10-04 06:54:21 -0600

    collapse

--- acordion.js
+++ acordion.js
@@ -1,7 +1,7 @@
 /** Accordion */
-function accordion (h3, setAttribute){
+function accordion (h3, setAttribute, userOptions){
     let titles = document.querySelectorAll('h3');
-    let p = document.querySelectorAll('.widget');
+    
     function color (event){
         event.target.setAttribute('class' , 'active');
     }
@@ -10,22 +10,52 @@ function accordion (h3, setAttribute){
          t.addEventListener('click' , color);
 
      }
- let linked = titles.length;
- linked.addEventListener('click' , function(){
-     p.toggle('p');
-     if (p.style.display === 'block'){
-         p.style.display ='none';
-     } else{
-         p.style.display = 'block';
-     }
- });
- 
-}
+    /// 
+/** Accordion */
 
-    function pop(){
-         if (p.style.display === 'block'){
-             p.style.display = 'none'
-         } else {
-             p.style.display = 'block';
-         }
+    //elementos del DOM
+    let titles = document.querySelectorAll('h3');
+    let content = document.querySelectorAll('div');
+    let p = document.querySelectorAll('.widget');
+    //objeto 
+    const options = Object.assign({
+        collapsible: false,
+        height: 'auto'
+      }, userOptions);
+  
+    //para cambiar de color con click
+    function color (event){
+        event.target.setAttribute('class' , 'active');
+        let header = event.target.nextElementSibling;
+        let headerclicked = event.target.className === 'active';
+        if (headerclicked){
+            if (options.collapsible){
+                event.target.className = '';
+                content.className = 'content-none';
+            }
+        } else{
+            collapseAll();
+                event.target.className = 'header-active';
+                content.className = 'content-visible';
+        }
+    }
+    
+     for (t of titles){
+         t.addEventListener('click' , color);
      }
+  
+    function collapseAll(){
+        for (let e of tittles){
+            tittles.className = '';
+        }
+        for (let c of content){
+            c.className = 'widget';
+        }
+    }
+    if (options.collapsible){
+        header[0].className = 'active';
+        content[0].className = 'content-block';
+    }
+}
+   
+
